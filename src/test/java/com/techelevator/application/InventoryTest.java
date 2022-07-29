1f6d2fc240c089fc5783ef26b6c88bbe8f181705
Andrew Briss <adbriss@gmail.com>
2022-07-29 10:37:18 -0400

    added test .csv file

--- src/test/java/com/techelevator/application/InventoryTest.java
+++ src/test/java/com/techelevator/application/InventoryTest.java
@@ -1,11 +1,39 @@
 package com.techelevator.application;
 
+import com.techelevator.ui.UserInput;
 import org.junit.Test;
 
-public class InventoryTest {
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class InventoryTest extends Inventory{
+    Inventory testInventory = new Inventory();
+
+    @Override
+    public void setInventory() {
+        String csvFileChoice = "toenails.csv";
+        File file = new File(csvFileChoice);
+        try {
+            Scanner data = new Scanner(file);
+            while (data.hasNextLine()) {
+                String line = data.nextLine();
+                String[] strArr = line.split(",");
+                ItemSlot itemSlot = new ItemSlot(strArr[0], strArr[1], new BigDecimal(strArr[2]), strArr[3]);
+                testInventory.getInventory().put(itemSlot.getSlot(), itemSlot);
+            }
+        } catch (IOException e) {
+            System.out.println("file not found");
+        }
+    }
+
     @Test
     public void test_set_inventory(){
         Inventory testInventory = new Inventory();
 
+
+        }
+
     }
-}
+
